6a8abcee35b9a4b27bf677f7c1daf0a252a0d4f0
cefimenda <jeffymenda@gmail.com>
2018-08-01 20:04:12 -0400

    created a function that displays event information in card format

--- assets/javascript/UITools.js
+++ assets/javascript/UITools.js
@@ -1,32 +1,32 @@
 Holder.addTheme('thumb', {
-    bg: '#55595c',
-    fg: '#eceeef',
-    text: 'Thumbnail'
-  });
-  
-  function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-      center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+  bg: '#55595c',
+  fg: '#eceeef',
+  text: 'Thumbnail'
+});
+
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+$("#favoritesButton").click(function () {
+  event.preventDefault()
+  $(".favoriteModal").modal('toggle')
+});
+$(".displayGif").on("click", ".card-img-top", function () {
+  var id = $(this).parent().attr('id')
+  console.log(id)
+  if ($(this).attr('src') == gifList[id].static) {
+    $(this).attr('src', gifList[id].url)
+  } else {
+    $(this).attr('src', gifList[id].static)
   }
-  
-  $("#favoritesButton").click(function () {
-    event.preventDefault()
-    $(".favoriteModal").modal('toggle')
-  });
-  $(".displayGif").on("click", ".card-img-top", function () {
-    var id = $(this).parent().attr('id')
-    console.log(id)
-    if ($(this).attr('src') == gifList[id].static) {
-        $(this).attr('src', gifList[id].url)
-    } else {
-        $(this).attr('src', gifList[id].static)
-    }
-  });
-  
-  function heartButtonClick(that) {
+});
+
+function heartButtonClick(that) {
   var id = $(that).parent().parent().attr('id')
   console.log(gifList[id].liked)
   console.log
@@ -36,8 +36,8 @@ Holder.addTheme('thumb', {
     $(".faves").empty()
     likedGifs[id] = gifList[id]
     for (var i in likedGifs) {
-        console.log("dumping")
-        dumpGif(likedGifs[i], $(".faves"))
+      console.log("dumping")
+      dumpGif(likedGifs[i], $(".faves"))
     }
     changeLikedButton('full', id)
   } else {
@@ -47,8 +47,41 @@ Holder.addTheme('thumb', {
     console.log('test')
     $(".faves").empty()
     for (var i in likedGifs) {
-        console.log("dumping")
-        dumpGif(likedGifs[i], $(".faves"))
+      console.log("dumping")
+      dumpGif(likedGifs[i], $(".faves"))
     }
     changeLikedButton('empty', id)
   }
+}
+
+function createCard(event) {
+  var card = $("<div>").addClass("card py-2")
+
+  var topRow = $("<div>").addClass("row m-3")
+
+  var nameCol = $("<div>").addClass("col-8")
+  var eventName = event.name
+  if (eventName == null) {
+    eventName = "Artists: "
+    for (var i in event.artistList) {
+      if (i == 0) {
+        eventName += event.artistList[i].name
+        continue
+      }
+      eventName += ", " + event.artistList[i].name
+    }
+  }
+  nameCol.text(eventName)
+  topRow.append(nameCol)
+
+  var dateCol = $("<div>").addClass("col-4")
+  dateCol.text(event.date)
+  topRow.append(dateCol)
+  card.append(topRow)
+
+  var bottomRow = $("<div>").addClass('row m-3')
+  var locationCol = $("<div>").addClass("col")
+  locationCol.text(event.venue.address)
+  bottomRow.append(locationCol)
+  card.append(bottomRow)
+}
