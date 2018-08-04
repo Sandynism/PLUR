5490136cd70ee4f462b6bbdd9e4d9a6411710861
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-08-04 13:14:34 -0400

    lightbox event

--- assets/javascript/plur.js
+++ assets/javascript/plur.js
@@ -23,6 +23,7 @@ function getEvents() {
         $("tbody").empty()
         for (var i in events){
             createCard(events[i])
+            // lightbox(events[i])
         }
     }).fail(function () {
         var response = eventsData
@@ -83,6 +84,10 @@ function createRow(event){
 $(function(){
     $(":submit").click(function(){
         event.preventDefault()
+        $('.carouselHeader').addClass('hide')
+        $('.festivalCard').addClass('hide')
+        $('.mapEvents').removeClass('hide')
+
         var state = $("input[type = search]").val()
         locationParameters.state = state
    
@@ -101,3 +106,79 @@ $(function(){
     })
 })
 
+function createEventName(event){
+    var eventName = event.name
+    if (eventName == null) {
+      eventName = "Artists: "
+      for (var i in event.artistList) {
+        if (i == 0) {
+          eventName += event.artistList[i].name
+          continue
+        }
+        eventName += ", " + event.artistList[i].name
+      }
+    }
+    return eventName
+  }
+  function createCard(event) {
+    var card = $("<div>").addClass("card eventCard bg-light p-3 w-100 my-3 ml-3 shadow-sm")
+    card.attr('data', JSON.stringify(event))
+  
+    var topRow = $("<div>").addClass("row")
+  
+    var nameCol = $("<div>").addClass("col-8")
+    var nameElem = $("<h5>").addClass("card-title")
+    var eventName = createEventName(event)
+    nameElem.text(eventName)
+    nameCol.append(nameElem)
+    topRow.append(nameCol)
+  
+    var dateCol = $("<div>").addClass("col-4 text-right")
+    var convertedDate = moment(event.date, "YYYY-MM-DD");
+    dateCol.text(moment(convertedDate).format("MM/DD/YY"))
+    topRow.append(dateCol)
+    card.append(topRow)
+  
+    var bottomRow = $("<div>").addClass('row')
+    var locationCol = $("<div>").addClass("col")
+    locationCol.text(event.venue.address)
+    bottomRow.append(locationCol)
+  
+    // YELP STUFF
+    // let yelpRestaurant = $("<div>").addClass("col")
+    // restaurants(event.venue.address)
+
+  
+    if (event.festivalInd == true) {
+      locationCol.addClass('col-10')
+      var badgeCol = $("<div>").addClass('col-2 text-right')
+      var badgeText = $("<p>").addClass("badge-warning text-black text-center")
+      badgeText.text('Electronic Festival')
+      badgeCol.append(badgeText)
+      bottomRow.append(badgeCol)
+    }
+  
+    card.append(bottomRow)
+    $(".cardDisplay").append(card)
+  }
+  
+  function lightbox(event) {
+    $(".lightbox").show()
+    var eventName = createEventName(event)
+    $(".lightbox-title").text(eventName)
+    $(".lightbox-date").text(event.date)
+    $(".lightbox-address").text(event.venue.address)
+    $(".lightbox-venue").text(event.venue.name)
+    let ticketURL = ("<a href="+event.ticketLink+" target='_blank' +>Ticket Purchase</a>")
+    $(".lightbox-ticketURL").html(ticketURL)
+    let ticketInfo = ("<a href="+event.link+" target='_blank' +>Event Information</a>")
+    $(".lightbox-infoURL").text(ticketInfo)
+  }
+  
+  $(function () {
+    console.log('running')
+    $(".cardDisplay").on("click", ".eventCard", function () {
+      var event = JSON.parse($(this).attr('data'))
+      lightbox(event)
+    })
+  })
