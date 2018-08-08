07d07876af524cb285feb7e98f254e137ac96487
Jack Yang <jackjoeng@gmail.com>
2018-08-07 20:55:34 -0400

    added share buttons on lightbox allow users to share events on social media

--- assets/javascript/plur.js
+++ assets/javascript/plur.js
@@ -265,6 +265,13 @@ function lightbox(event) {
     $(".lightbox-infoURL").html(ticketInfo)
     let weatherInfo = (`<div class="lightbox-weather"><div id="result_lb" style="display: none"><div id="icon"></div><div id="city"><strong></strong></div><div id="weather"></div></div>`)
     $(".lightbox-weather").html(weatherInfo)
+    let shareInfo = (`<h4>Share this event:</h4> <a href="mailto:?Subject=Simple Share Buttons&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20` + event.ticketLink + `"><img src="https://simplesharebuttons.com/images/somacro/email.png" width="30px" alt="Email" /></a>
+    <a href="http://www.facebook.com/sharer.php?u=` + event.ticketLink+ `" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/facebook.png" width="30px" alt="Facebook" /></a>
+    <a href="https://plus.google.com/share?url=` + event.ticketLink+ `" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/google.png" width="30px" alt="Google" /></a>
+    <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=` + event.ticketLink + `https://simplesharebuttons.com" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/linkedin.png" width="30px" alt="LinkedIn" /></a>
+    <a href="http://www.tumblr.com/share/link?url=` + event.ticketLink+ `&amp;title=Simple Share Buttons" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/tumblr.png" width="30px" alt="Tumblr" /></a>
+    <a href="https://twitter.com/share?url=` + event.ticketLink+ `&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/twitter.png" width="30px" alt="Twitter" /></a>`)
+    $(".lightbox-share").html(shareInfo)
     restaurants(event.venue.address)
     getWeather(event.venue.address)
 }
