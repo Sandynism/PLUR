cd8f51b7d53adb9d324181c1dcdd0c69c0896f6a
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-08-10 07:53:06 -0400

    minor changes

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -8,9 +8,8 @@ var config = {
     projectId: "plur-9d2d6",
     storageBucket: "plur-9d2d6.appspot.com",
     messagingSenderId: "236610079904"
-};
-firebase.initializeApp(config);
-
+}
+firebase.initializeApp(config)
 
 
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
@@ -20,7 +19,7 @@ var uiConfig = {
             return true;
         },
         uiShown: function () {
-            document.getElementById('loader').style.display = 'none';
+            document.getElementById('loader').style.display = 'none'
         }
     },
     signInFlow: 'popup',
@@ -31,33 +30,30 @@ var uiConfig = {
     privacyPolicyUrl: 'privacy.html'
 };
 
-    ui.start('#firebaseui-auth-container', uiConfig);
+    ui.start('#firebaseui-auth-container', uiConfig)
 
-
+//on login button click, remove class
 $('.login').on('click', function () {
-    console.log('clicked logged in')
     $('.logout').removeClass('hide')
 })
 
+//on logout click, sign the user out from firebase
 $('.logout').on('click', function (event) {
     event.preventDefault()
-    console.log('clicked logged out')
     firebase.auth().signOut().then(function () {
-        console.log('logged out')
         // Sign-out successful.
         $('.login').removeClass('hide')
         $('.logout').addClass('hide')
     }).catch(function (error) {
         console.log(error)
         // An error happened.
-    });
+    })
 
 })
 
 //firebase authentication logout
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        console.log(user.displayName)
         $('.login').addClass('hide')
         $('.logout').removeClass('hide')
         let dropdown = $('.fa-user-circle')
@@ -65,9 +61,9 @@ firebase.auth().onAuthStateChanged(function (user) {
         dropdown.text(initials)
 
     } else {
-        console.log('not logged in')
+        console.log('User is not logged in')
     }
-});
+})
 
 
 window.fbAsyncInit = function () {
@@ -76,31 +72,31 @@ window.fbAsyncInit = function () {
         cookie: true,
         xfbml: true,
         version: '{v3.1}'
-    });
+    })
 
-    FB.AppEvents.logPageView();
+    FB.AppEvents.logPageView()
 
-};
+}
 
 FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
-});
+    statusChangeCallback(response)
+})
 
 
 
 (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
+    var js, fjs = d.getElementsByTagName(s)[0]
     if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+    js = d.createElement(s); js.id = id
+    js.src = "https://connect.facebook.net/en_US/sdk.js"
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
 
 
 function checkLoginState() {
     FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
+        statusChangeCallback(response)
+    })
 }
 
 
